dcf6342b9aadea60b224774d27f443b6c2c19a70
Meysam Karimi <meysam.karimi.ebox@gmail.com>
2023-02-18 23:53:13 +0330

    Adds new integration test

---
+++ bank-service/src/test/java/com/meysam/emulator/ControllerTests.java
@@ -0,0 +1,25 @@
+package com.meysam.emulator;
+
+import com.meysam.emulator.card.controller.BankController;
+import com.meysam.emulator.card.dto.DepositRequest;
+import com.meysam.emulator.card.dto.DepositResponse;
+import com.meysam.emulator.card.exception.CashWithdrawFailureException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+
+import java.math.BigDecimal;
+
+@SpringBootTest
+public class ControllerTests {
+
+    @Autowired
+    BankController bankController;
+
+    @Test
+    public void cashDeposit_validAmount() throws CashWithdrawFailureException {
+        DepositRequest depositRequest = new DepositRequest(new BigDecimal(1000), "546554");
+        ResponseEntity<DepositResponse> responseEntity = bankController.cashDeposit(depositRequest);
+    }
+}
