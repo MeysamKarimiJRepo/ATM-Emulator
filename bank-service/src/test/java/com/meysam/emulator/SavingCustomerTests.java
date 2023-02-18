1ecbfe8b09bec26935346ac294235edb89a46cd1
Meysam Karimi <meysam.karimi.ebox@gmail.com>
2023-02-18 21:58:17 +0330

    Adds the failure test

--- bank-service/src/test/java/com/meysam/emulator/SavingCustomerTests.java
+++ bank-service/src/test/java/com/meysam/emulator/SavingCustomerTests.java
@@ -12,17 +12,22 @@ public class SavingCustomerTests {
     CustomerRepository customerRepository;
 
     @Test
-    public void saveCustomer() {
-        saveCustomer("fatherName", "myFingerPrint", "myLastName", "myFirstName", "nationalCode");
+    public void saveCustomer_duplicatedNcode_failes() {
+        Customer customer = new Customer();
+        customer.setFatherName("fatherName");
+        customer.setFingerPrint("myFin");
+        customer.setLastName("myLast");
+        customer.setFirstName("firstName");
+        customer.setNationalCode("55845");
+        customerRepository.save(customer);
+
+        Customer customer1 = new Customer();
+        customer1.setFatherName("fatherName");
+        customer1.setFingerPrint("myFin");
+        customer1.setLastName("myLast");
+        customer1.setFirstName("firstName");
+        customer1.setNationalCode("55845");
+        customerRepository.save(customer1);
     }
 
-    private void saveCustomer(String fatherName, String finger, String lastName, String firstName, String nCode) {
-        Customer customer = new Customer();
-        customer.setFatherName(fatherName);
-        customer.setFingerPrint(finger);
-        customer.setLastName(lastName);
-        customer.setFirstName(firstName);
-        customer.setNationalCode(nCode);
-        customerRepository.save(customer);
-    }
 }
